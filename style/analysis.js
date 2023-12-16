c13f975267f2dae27dc8e7bca9662061c166e22b
zhuifan1 <140639499+zhuifan1@users.noreply.github.com>
2023-12-16 19:24:03 +0800

    Add files via upload

---
+++ style/analysis.js
@@ -0,0 +1,13 @@
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="https://total.idc-status.com/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', '1xDkvymMyOBNlLK']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.async=true;
+  g.src=u+'matomo.js';
+  s.parentNode.insertBefore(g,s);
+})();
